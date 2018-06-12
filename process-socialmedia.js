882e7b56ecc1b6fcc0c919ff3d08e394b606d7db
Peter West <peter.g.west@gmail.com>
2018-06-12 22:59:27 +0100

    fixes

--- process-socialmedia.js
+++ process-socialmedia.js
@@ -66,7 +66,7 @@ const processTwitter = id => {
 			} while (true);
 
 			if (Object.keys(possibleOfficialAccounts).length === 0) {
-				console.error('failed to parse');
+				console.error(`> failed to parse ${id}`);
 				return;
 			}
 
@@ -283,11 +283,124 @@ const processFacebook = id => {
 		});
 };
 
+
+const processWeirdFile = id => { // for stars4all Facebook.xlsx
+	return fs.readFile(`${__dirname}/data/socialmedia/${id}.xlsx`)
+		.then(data => {
+			let workbook = xlsx.read(data, { cellDates: true }),
+				Edges = workbook.Sheets.Edges,
+				Vertices = workbook.Sheets.Vertices;
+
+			if (Vertices.AP2.v !== 'Custom Menu Item Text') {
+				throw new Error(`${id} is not facebook data, skipping...`);
+			}
+			console.log(`> detected weird facebook data ${id}`);
+			
+			let vertices = [],
+				edges = [],
+				i = 3,
+				officialAccount,
+				dateStart,
+				dateEnd,
+				possibleOfficialAccounts = {},
+				defaultVals = { engagers: 0, posts: 0, replies: 0, mentions: 0, favorites: 0, shares: 0, comments: 0 },
+				totals = Object.assign({}, defaultVals),
+				months = {};
+
+			do { // go through each row of the sheet
+				let vertex = getVal(Vertices[`A${i}`]),
+					type = getVal(Vertices[`BL${i}`]),
+					author = getVal(Vertices[`BU${i}`]),
+					date = getVal(Vertices[`BV${i}`]) || getVal(Vertices[`CG${i}`]),
+					likes = getVal(Vertices[`BY${i}`]),
+					shares = getVal(Vertices[`CA${i}`]) || 0,
+					comments = getVal(Vertices[`BZ${i}`]),
+					content = getVal(Vertices[`BS${i}`]);
+				if (!vertex) { break; } // reached end of sheet
+				if (author) {
+					if (!possibleOfficialAccounts[author]) { possibleOfficialAccounts[author] = 0; }
+					possibleOfficialAccounts[author]++;
+				}
+				if (date) {
+					if (!dateStart || date < dateStart) { dateStart = date; }
+					if (!dateEnd || date > dateEnd) { dateEnd = date; }
+				}
+				vertices.push({ vertex, type, date, author, likes, shares, comments, content });
+				i++;
+			} while (true);
+			i = 3;
+			do { // go through each row of the sheet
+				let vertex1 = getVal(Edges[`A${i}`]),
+					vertex2 = getVal(Edges[`B${i}`]),
+					date = getVal(Edges[`U${i}`]);
+				if (!vertex1) { break; } // reached end of sheet
+				edges.push({ vertex1, vertex2, date });
+				i++;
+			} while (true);
+
+			officialAccount = Object.entries(possibleOfficialAccounts)
+				.sort((a, b) => a[0] - b[0])
+				[0][0];
+
+			let date = moment(dateStart).startOf('month').toDate();
+			date = moment(dateStart).startOf('month').toDate();
+			while (date <= dateEnd) {
+				let month = moment(date).format('YYYY-MM');
+				months[month] = Object.assign({ month }, defaultVals);
+				date = moment(date).add(1, 'month').toDate();
+			}
+			let posts = [];
+			vertices.forEach(vertex => {
+				let month = moment(vertex.date).format('YYYY-MM');
+				if (vertex.type === 'Post' && vertex.author === officialAccount) {
+					[months[month], totals].forEach(o => {
+						o.posts += 1;
+						o.favorites += vertex.likes;
+						o.shares += vertex.shares;
+					});
+					posts.push({ content: vertex.content, favorites: vertex.likes, 
+						shares: vertex.shares, comments: vertex.comments });
+				}
+				if (vertex.type === 'User') {
+					if (vertex.vertex !== officialAccount) {
+						totals.engagers += 1;
+					}
+				}
+				if (vertex.type === 'Comment' && vertex.author === officialAccount) {
+					[months[month], totals].forEach(o => o.replies += 1);
+				}
+				if (['Comment', 'Post'].includes(vertex.type) && vertex.author !== officialAccount) {
+					[months[month], totals].forEach(o => o.comments += 1);
+				}
+			});
+			edges.forEach(edge => {
+				if (edge.vertex2 === officialAccount) {
+					let month = moment(edge.date).format('YYYY-MM');
+					[months[month], totals].forEach(o => o.mentions += 1);
+				}
+			});
+
+			let mostFavorited = posts.sort((a, b) => b.favorites - a.favorites).slice(0, 10),
+				mostDiscussed = posts.sort((a, b) => b.comments - a.comments).slice(0, 10),
+				mostShared = posts.sort((a, b) => b.shares - a.shares).slice(0, 10);
+
+			months = Object.keys(months).sort().map(m => months[m]);
+
+			let project = { id, type: 'facebook', officialAccount, months, totals,
+				mostFavorited, mostDiscussed, mostShared };
+
+			let js = `window.socialmedia.push(${JSON.stringify(project, null, '\t')});`;
+			//js = js.replace(/\[[^\[\]]*\]/g, match => match.replace(/\s+/g, ' ')); // remove excessive whitespce
+			return fs.writeFile(`${__dirname}/build/accounts/${id}.js`, js, 'utf8');
+		});
+};
+
 const run = (id) => {
 	id = id.replace(/.xlsx$/, ''); // string extension
 	console.log(`Parsing ${id}...`);
 	console.time(`> successfully parsed ${id}`);
-	return processFacebook(id)
+	return processWeirdFile(id)
+		.catch(() => processFacebook(id))
 		.catch(() => processTwitter(id))
 		.then(() => console.timeEnd(`> successfully parsed ${id}`))
 		.catch(err => console.error(`[${id}] ERROR:`, err));
